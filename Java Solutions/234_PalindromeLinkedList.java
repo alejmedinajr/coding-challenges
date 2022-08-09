4887abd7c170305ad905dad22616515d8445bfaa
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2022-08-09 15:50:59 -0500

    Added Author comments
    Author comments and short description along with link was added to 13. and 234.

--- Java Solutions/234_PalindromeLinkedList.java
+++ Java Solutions/234_PalindromeLinkedList.java
@@ -1,3 +1,10 @@
+/**
+ * Given the head of a singly linked list, return true if it is a palindrome. 
+ * 
+ * @author Alejandro Medina (@alejmedinajr)
+ * 
+ * Can be found at: https://leetcode.com/problems/palindrome-linked-list/
+ */
 public class Solution {
 	private ListNode left = null;
 	public boolean isPalindrome(ListNode head) {
