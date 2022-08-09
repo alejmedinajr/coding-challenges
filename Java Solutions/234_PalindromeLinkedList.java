9659bb3ba86b063b9e182b98c85d16ac17e503db
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2022-08-09 15:45:00 -0500

    #2 Final Solution
    Fixed problem involving the way the file was created, also put final solution.

--- Java Solutions/234_PalindromeLinkedList.java
+++ Java Solutions/234_PalindromeLinkedList.java
@@ -1,31 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 public class Solution {
-    private ListNode left = null;
-    public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-        left = head;
-        return isP(head);
-    }
-    private boolean isP(ListNode right) {
-        if (right == null) {
-            return true;
-        }
-        if (!isP(right.next)) {
-            return false;
-        }
-        boolean result = left.val == right.val;
-        left = left.next;
-        return result;
-    }
+	private ListNode left = null;
+	public boolean isPalindrome(ListNode head) {
+		if (head == null || head.next == null) return true; // an empty linked list or a linked list of size one is a palindrome
+	
+		left = head;
+		
+		return isP(head);
+	}
+	private boolean isP(ListNode right) {
+		boolean result = null;
+		
+		if (right == null) return true;
+		if (!isP(right.next)) return false;
+		
+		result = left.val == right.val;
+		left = left.next;
+		
+		return result;
+	}
 }	
