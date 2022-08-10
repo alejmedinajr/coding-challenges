1451d5b6e6d876b566a322610e0200cf03e6c7f7
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2022-08-10 18:06:08 -0500

    Update 2357_MakeArrayZeroBySubtractingEqualAmounts.java
    A final solution

--- Java Solutions/2357_MakeArrayZeroBySubtractingEqualAmounts.java
+++ Java Solutions/2357_MakeArrayZeroBySubtractingEqualAmounts.java
@@ -1,33 +1,43 @@
+/**
+ * You are given a non-negative integer array nums. In one operation, you must:
+ * Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+ * Subtract x from every positive element in nums.
+ * Return the minimum number of operations to make every element in nums equal to 0.
+ *
+ * @author Alejandro Medina (@alejmedinajr)
+ *
+ * Can be found at: https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts/
+ */
 class Solution {
-    public int minimumOperations(int[] nums) {
-        int numOperations = 0;
-        boolean done = false;
-        
-        while(!done) {
-            int min = 1000;
+	public int minimumOperations(int[] nums) {
+		int numOperations = 0;
+		boolean done = false;
 
-            for(int n: nums){
-                if(n != 0) {
-                    min = Math.min(min, n);
-                }
-            }
-            boolean enteredChange = false;
-            for(int i = 0; i < nums.length; i++){
-                if(nums[i] != 0) {
-                    enteredChange = true;
-                    nums[i] -= min;   
-                }
-            }
-            
-            int sum = 0;
-            for(int n: nums) {
-                sum += n;       
-            }
-            
-            done = (sum == 0);
-            if(enteredChange) numOperations++;
- 
-        }
-        return numOperations;
-    }
+		while(!done) {
+			int min = 1000;
+
+			for(int n: nums){
+				if(n != 0) {
+					min = Math.min(min, n);
+				}
+			}
+			boolean enteredChange = false;
+			for(int i = 0; i < nums.length; i++){
+				if(nums[i] != 0) {
+					enteredChange = true;
+					nums[i] -= min;   
+				}
+			}
+
+			int sum = 0;
+			for(int n: nums) {
+				sum += n;       
+			}
+
+			done = (sum == 0);
+			if(enteredChange) numOperations++;
+
+		}
+		return numOperations;
+	}
 }
